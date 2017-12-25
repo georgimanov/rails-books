2d0ffc1ee30a9e06deed1c55bc0fa01d2abbf214
Georgi Manov <georgimanov@gmail.com>
2017-12-25 18:54:06 +0200

    implemented edit/delete operations

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,14 +1,18 @@
 class BooksController < ApplicationController
+
   def index
     @books = Book.all
   end
+
   def show
     id = params[:id]
     @book = Book.find(id)
   end
+
   def new
     @book = Book.new
   end
+
   def create
    params.require(:book)
    permitted = params[:book].permit(:title,:genre,:publish_date,:isbn_number,:description)
@@ -16,4 +20,25 @@ class BooksController < ApplicationController
    flash[:notice] = "#{@book.title} was successfully created."
    redirect_to books_path
   end
+
+  def edit
+    @book = Book.find params[:id]
+  end
+
+  def update
+    @book = Book.find params[:id]
+    params.require(:book)
+    permitted = params[:book].permit(:title,:genre,:publish_date,:isbn_number,:description)
+    @book.update_attributes!(permitted)
+    flash[:notice] = "#{@book.title} was successfully updated."
+    redirect_to book_path(@book)
+  end
+
+  def destroy
+    @book = Book.find params[:id]
+    @book.destroy
+    flash[:notice] = "Book '#{@book.title}' deleted."
+    redirect_to books_path
+  end
+
 end
