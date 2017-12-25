e13b379db51142ea5134190a1ea566f72b017b0e
Georgi Manov <georgimanov@gmail.com>
2017-12-25 18:41:06 +0200

    implemented create new book

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -6,4 +6,14 @@ class BooksController < ApplicationController
     id = params[:id]
     @book = Book.find(id)
   end
+  def new
+    @book = Book.new
+  end
+  def create
+   params.require(:book)
+   permitted = params[:book].permit(:title,:genre,:publish_date,:isbn_number,:description)
+   @book = Book.create!(permitted)
+   flash[:notice] = "#{@book.title} was successfully created."
+   redirect_to books_path
+  end
 end
