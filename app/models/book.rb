c6f1696e934a640965e30528cce2387e4c4479eb
Georgi Manov <georgimanov@gmail.com>
2018-01-20 11:01:33 +0200

    added validation for model

--- app/models/book.rb
+++ app/models/book.rb
@@ -1,5 +1,11 @@
 class Book < ActiveRecord::Base
+
   def self.all_genres
     ['Science fiction','Drama','Action and Adventure','Romance','Mystery','Horror']
   end
+
+  validates :title, :presence => true
+  validates :publish_date, :presence => true
+  validates :genre, :inclusion => {:in => Book.all_genres}
+
 end
